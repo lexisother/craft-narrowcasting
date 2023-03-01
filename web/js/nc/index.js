8b57f5e9cf567394d21c565df2911e19304c26eb
Alyxia Sother <alyxia@riseup.net>
2023-03-01 11:34:59 +0100

    [templates/nc] BRKNC-14: Utilize Swiper

---
+++ web/js/nc/index.js
@@ -0,0 +1,7 @@
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.esm.browser.min.js'
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+})
