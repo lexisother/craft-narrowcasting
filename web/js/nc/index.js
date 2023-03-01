1b74e89708082dabc37f5653b91c69b725c6d4e6
Alyxia Sother <alyxia@riseup.net>
2023-03-01 16:32:50 +0100

    [js/nc] BRKNC-26: Add arrow controls

--- web/js/nc/index.js
+++ web/js/nc/index.js
@@ -1,7 +1,26 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.esm.browser.min.js';
 
-const _swiper = new Swiper('.swiper', {
-  // Optional parameters
+// Notes {{{
+// TODO: Introduce the LocalStorage manager
+// TODO: Introduce the entity system and use that for controlling handlers
+// TODO: Introduce the dynamic data system and store the state in localStorage
+// }}}
+
+/** @type {import("swiper").Swiper} */
+export let swiper;
+
+swiper = new Swiper('.swiper', {
   direction: 'horizontal',
   loop: true,
+});
+
+// Controls the left-right functionality
+let ArrowLeft = () => swiper.slidePrev();
+let ArrowRight = () => swiper.slideNext();
+const handler = { ArrowLeft, ArrowRight };
+document.addEventListener('keydown', (e) => {
+  const k = e.key;
+  if (handler.hasOwnProperty(k)) {
+    handler[k](k);
+  }
 });
